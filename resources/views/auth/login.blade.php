96ea5cd7a14daa8ec36bc1dc056d1c876c62c4f5
Rahmad Wahyudi <>
2017-05-17 11:25:01 +0700

    Init

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap Material Design -->
+    <link href="{{ asset('css/bootstrap-material-design.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/ripples.min.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+
+    <!-- jQuery -->
+    <script src="{{ asset('js/jquery-1.10.2.min.js') }}"></script>
+
+    <!-- Custom CSS -->
+    <style type="text/css">
+        .panel-login {
+            margin-top: 10em;
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default panel-login">
+                    <div class="panel-body">
+                        <center>
+                            <h3>{{ config('app.name', 'Laravel') }}</h3>
+                        </center>
+
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+                                <div class="col-md-10 col-md-offset-1">
+                                    <label for="email" class="control-label">username</label>
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+                                <div class="col-md-10 col-md-offset-1">
+                                    <label for="password" class="control-label">password</label>
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-10 col-md-offset-1">
+                                    <button type="submit" class="btn btn-primary btn-block">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Twitter Bootstrap -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    <!-- Material Design for Bootstrap -->
+    <script src="{{ asset('js/material.min.js') }}"></script>
+    <script src="{{ asset('js/ripples.min.js') }}"></script>
+    <script>
+        $.material.init();
+    </script>
+</body>
+</html>
